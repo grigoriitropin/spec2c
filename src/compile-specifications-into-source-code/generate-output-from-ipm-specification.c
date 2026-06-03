93ed0f251731076fe94061470e62ed920e7d3a03
grigoriitropin <grigoriitropin@gmail.com>
2026-06-03 16:43:39 +0300

    extract hyphen translator as shared helper to fit function limits

--- src/compile-specifications-into-source-code/generate-output-from-ipm-specification.c
+++ src/compile-specifications-into-source-code/generate-output-from-ipm-specification.c
@@ -44,15 +44,7 @@ void write_component_header_with_prototypes(const ipm_spec_t *spec, const char *
     if (!hdr) report_fatal_error_and_exit("cannot open header file");
 
     char guard[256];
-    int gk = 0;
-    while (modname[gk]) {
-        if (modname[gk] == '-')
-            guard[gk] = '_';
-        else
-            guard[gk] = modname[gk];
-        gk++;
-    }
-    guard[gk] = 0;
+    translate_hyphens_in_name(guard, modname);
     strcat(guard, "_H");
 
     fprintf(hdr, "/* Auto-generated from %s.ipm */\n", modname);
@@ -132,6 +124,18 @@ static void generate_auto_entry_point_code(FILE *out_fp, const ipm_spec_t *spec)
     fprintf(out_fp, "    return %s();\n}\n", entry);
 }
 
+static void translate_hyphens_in_name(char *dst, const char *src) {
+    int i = 0;
+    while (src[i]) {
+        if (src[i] == '-')
+            dst[i] = '_';
+        else
+            dst[i] = src[i];
+        i++;
+    }
+    dst[i] = 0;
+}
+
 void handle_ipm_spec_emit_code(const ipm_spec_t *spec, const char *out_path, int is_library) {
     if (is_library && out_path)
         emit_ipm_component_header_file(spec, out_path);
@@ -140,15 +144,7 @@ void handle_ipm_spec_emit_code(const ipm_spec_t *spec, const char *out_path, int
     if (modname[0] && out_path) {
         char hdr_path[4096];
         char cmod[256];
-        int ck = 0;
-        while (modname[ck]) {
-            if (modname[ck] == '-')
-                cmod[ck] = '_';
-            else
-                cmod[ck] = modname[ck];
-            ck++;
-        }
-        cmod[ck] = 0;
+        translate_hyphens_in_name(cmod, modname);
         const char *slash = strrchr(out_path, '/');
         int dirlen = slash ? (int)(slash - out_path + 1) : 0;
         snprintf(hdr_path, sizeof(hdr_path), "%.*s%s.h", dirlen, out_path, cmod);
@@ -160,15 +156,7 @@ void handle_ipm_spec_emit_code(const ipm_spec_t *spec, const char *out_path, int
 
     if (modname[0]) {
         char cname[256];
-        int ci = 0;
-        while (modname[ci]) {
-            if (modname[ci] == '-')
-                cname[ci] = '_';
-            else
-                cname[ci] = modname[ci];
-            ci++;
-        }
-        cname[ci] = 0;
+        translate_hyphens_in_name(cname, modname);
         fprintf(out_fp, "#include <string.h>\n#include <stdio.h>\n#include <stdlib.h>\n#include <cjson/cJSON.h>\n");
         fprintf(out_fp, "#include \"%s.h\"\n\n", cname);
     }
