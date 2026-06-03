22ebc837b1fda70cab7635e7d2bf91a56101800e
grigoriitropin <grigoriitropin@gmail.com>
2026-06-03 23:16:59 +0300

    split: extract scan_dir as recursive_scan_source_directory (43→47 lines standalone)

--- src/compile-specifications-into-source-code/enforce-structural-rules-for-code/verify-structural-source-code-rules.c
+++ src/compile-specifications-into-source-code/enforce-structural-rules-for-code/verify-structural-source-code-rules.c
@@ -264,6 +264,51 @@ static void search_for_unused_function_code(fn_entry_t *fns, int fn_qty, const c
 }
 static void scan_source_for_undocumented_flags(const char *srcdir);
 
+static void recursive_scan_source_directory(const char *dirpath,
+    fn_entry_t *fns, int *fn_qty, inc_entry_t *incs, int *inc_qty) {
+    DIR *d = opendir(dirpath); if (!d) return;
+    int file_cnt = 0;
+    struct dirent *de;
+    while ((de = readdir(d)) != NULL) {
+        if (de->d_name[0] == '.') continue;
+        char sub[8192]; snprintf(sub, sizeof(sub), "%s/%s", dirpath, de->d_name);
+        struct stat st;
+        if (stat(sub, &st) != 0) continue;
+        if (S_ISDIR(st.st_mode)) {
+            validate_name_against_soul_rules("directory", de->d_name, sub);
+            recursive_scan_source_directory(sub, fns, fn_qty, incs, inc_qty);
+            continue;
+        }
+        if (!match_source_code_header_filename(de->d_name)) {
+            size_t dn_len = strlen(de->d_name);
+            int is_ipm = dn_len > 4 && !strcmp(de->d_name + dn_len - 4, ".ipm");
+            if (!is_ipm) {
+                if (!check_non_source_file_allowlist(de->d_name)) {
+                    fprintf(stderr, "FATAL: non-source file in %s: %s\n", dirpath, de->d_name);
+                    exit(1);
+                }
+                continue;
+            }
+        }
+        if (!match_name_against_bootstrap_list(de->d_name)) {
+            size_t dn_len2 = strlen(de->d_name);
+            int is_ipm2 = dn_len2 > 4 && !strcmp(de->d_name + dn_len2 - 4, ".ipm");
+            if (!is_ipm2)
+                report_violation_with_actionable_hint(ERR_NOT_IN_BOOTSTRAP_WHITELIST, sub, 0, 0, NULL);
+        }
+        file_cnt++;
+        char fname[256]; snprintf(fname, sizeof(fname), "%s", de->d_name);
+        char *dot = strrchr(fname, '.'); if (dot) *dot = 0;
+        validate_name_against_soul_rules("file", fname, sub);
+        int is_c = !strcmp(de->d_name + strlen(de->d_name) - 2, ".c");
+        int is_source = is_c || (strlen(de->d_name) > 4 && !strcmp(de->d_name + strlen(de->d_name) - 4, ".ipm"));
+        check_single_file_for_violations(sub, is_c, is_source, fns, fn_qty, incs, inc_qty);
+    }
+    closedir(d);
+    if (file_cnt > MAX_FILES_PER_DIR)
+        report_violation_with_actionable_hint(ERR_TOO_MANY_FILES_IN_DIR, dirpath, file_cnt, MAX_FILES_PER_DIR, NULL);
+}
+
 void enforce_all_source_code_rules(const char *srcdir) {
     read_allowed_names_from_file(srcdir);
     read_banned_patterns_from_file(srcdir);
@@ -271,50 +316,7 @@ void enforce_all_source_code_rules(const char *srcdir) {
     load_bootstrap_whitelist_from_disk(srcdir);
     fn_entry_t fns[512]; int fn_qty = 0;
     inc_entry_t incs[128]; int inc_qty = 0;
-    void scan_dir(const char *dirpath) {
-        DIR *d = opendir(dirpath); if (!d) return;
-        int file_cnt = 0;
-        struct dirent *de;
-        while ((de = readdir(d)) != NULL) {
-            if (de->d_name[0] == '.') continue;
-            char sub[8192]; snprintf(sub, sizeof(sub), "%s/%s", dirpath, de->d_name);
-            struct stat st;
-            if (stat(sub, &st) != 0) continue;
-            if (S_ISDIR(st.st_mode)) {
-                validate_name_against_soul_rules("directory", de->d_name, sub);
-                scan_dir(sub); continue;
-            }
-            if (!match_source_code_header_filename(de->d_name)) {
-                size_t dn_len = strlen(de->d_name);
-                int is_ipm = dn_len > 4 && !strcmp(de->d_name + dn_len - 4, ".ipm");
-                if (!is_ipm) {
-                    if (!check_non_source_file_allowlist(de->d_name)) {
-                        fprintf(stderr, "FATAL: non-source file in %s: %s\n", dirpath, de->d_name);
-                        exit(1);
-                    }
-                    continue;
-                }
-            }
-            if (!match_name_against_bootstrap_list(de->d_name)) {
-                size_t dn_len2 = strlen(de->d_name);
-                int is_ipm2 = dn_len2 > 4 && !strcmp(de->d_name + dn_len2 - 4, ".ipm");
-                if (!is_ipm2)
-                    report_violation_with_actionable_hint(ERR_NOT_IN_BOOTSTRAP_WHITELIST, sub, 0, 0, NULL);
-            }
-            file_cnt++;
-            char fname[256]; snprintf(fname, sizeof(fname), "%s", de->d_name);
-            char *dot = strrchr(fname, '.'); if (dot) *dot = 0;
-            validate_name_against_soul_rules("file", fname, sub);
-            int is_c = !strcmp(de->d_name + strlen(de->d_name) - 2, ".c");
-            int is_source = is_c || (strlen(de->d_name) > 4 && !strcmp(de->d_name + strlen(de->d_name) - 4, ".ipm"));
-            check_single_file_for_violations(sub, is_c, is_source, fns, &fn_qty, incs, &inc_qty);
-        }
-        closedir(d);
-        if (file_cnt > MAX_FILES_PER_DIR) {
-            report_violation_with_actionable_hint(ERR_TOO_MANY_FILES_IN_DIR, dirpath, file_cnt, MAX_FILES_PER_DIR, NULL);
-        }
-    }
-    scan_dir(srcdir);
+    recursive_scan_source_directory(srcdir, fns, &fn_qty, incs, &inc_qty);
     search_for_unused_function_code(fns, fn_qty, srcdir);
     int main_count = 0;
     for (int i = 0; i < fn_qty; i++)
